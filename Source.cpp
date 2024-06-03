a1dfbd20a161f1bc77b27f88fc21d881dde321fc
Avictor <161009472+AvaHtm@users.noreply.github.com>
2024-06-03 23:49:59 +0330

    Add files via upload
    برنامه ای که دو مقدار از ورودی بگیرد و انهارا با هم جمع/از هم کم /یا به توان یکدیگر برساند و در صورت وارد کردن عملگر اشتباه پیغام مناسب بدهد .

---
+++ Source.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<cstring>//for pow function
+using namespace std;
+struct calculator{
+	int leftvalue , rightvalue;
+	char Operator;
+	void start()
+	{
+		cout<<"enter :";
+		cin>>leftvalue>>Operator>>rightvalue;
+	}
+	int calculate()
+	{
+		int result;
+		if(Operator=='+')
+		{
+			result=leftvalue+rightvalue;
+			cout<<"+:"<<result<<endl;
+		}
+		else if(Operator=='-')
+		{
+			result=leftvalue-rightvalue;
+			cout<<"-:"<<result<<endl;
+		}
+		else if(Operator=='^')
+		{
+			result=pow(leftvalue,rightvalue);
+			cout<<"^:"<<result<<endl;
+		}
+		else
+		{
+		    cout << "Bad operator"<<endl;
+            result = 0;
+		}
+		return result;
+	}
+};
+int main()
+{
+	calculator c;
+	c.start();
+	int result=c.calculate();
+	return 0;
+}
